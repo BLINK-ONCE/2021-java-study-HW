31914555dcc6d74600053615acef7b223eebba25
BLINK-ONCE <mkmkqoa@gmail.com>
2021-01-04 22:21:38 +0900

    2weeks

---
+++ tutoring/nextInt().java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);//ctrl + Shift + Enter
+        int a = scanner.nextInt();//integer only
+        int b = scanner.nextInt();//integer only
+
+        System.out.println(a - b);
+    }
+}
---
+++ tutoring/scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);//ctrl + Shift + Enter
+        String a = scanner.next();//declare String
+        int b = scanner.nextInt();//integer only
+
+        System.out.print(a + " " + b);
+
+        System.out.println();
+
+    }
+}
