306b902b45e98b3ab0a2973878e25b3cc6d1f439
BLINK-ONCE <mkmkqoa@gmail.com>
2021-04-13 12:22:30 +0900

    Add files

--- 5주차/5weeks/refator string calculator/gradle/src/main/java/string-calculator/domain/ServiceStarter.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class ServiceStarter {
-    public static void main(String[] args) throws IOException {
-        Formula formula = new Formula();
-        formula.inputFormula();
-    }
-}
